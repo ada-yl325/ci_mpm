25b45a3d4ff871bde00e714219a99e0872804632
Yixuan Li <yixuan.li125@imperial.ac.uk>
2025-10-15 15:30:28 +0100

    update function and constants

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,6 @@
+from functools import cache
 
-__all__ = ['my_sum']
+__all__ = ['my_sum', 'factorial']
 
 
 def my_sum(iterable):
@@ -7,3 +8,14 @@ def my_sum(iterable):
     for i in iterable:
         tot += i
     return tot
+
+
+@cache
+def factorial(n):
+    return n * factorial(n-1) if n else 1
+
+
+@cache
+def rsum(n):
+    t = factorial(4*n)*(1103+26390*n)/(factorial(n)**4*396**(4*n))
+    return t + rsum(n-1) if n else t
